cbedba5b78844b6542413eee98d777d99517a50d
Thevar Kandda Kumaran Murugan <67261354+Kumaran-Thevar@users.noreply.github.com>
2022-11-03 12:55:56 +0530

    Create add1.java

---
+++ add1.java
@@ -0,0 +1,10 @@
+public class add1 {
+public static void main(string args[])
+{
+int a,b,sum;
+a = Integer.parseInt(args[0]);
+b = Integer.parseInt(args[1]);
+sum = a+b;
+System.out.println(sum);
+}
+}
